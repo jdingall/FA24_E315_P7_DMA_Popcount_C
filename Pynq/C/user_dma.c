afce3a8bfb729f034d4222721b337037b3487ea2
Andrew Lukefahr <andrew@lukefahr.org>
2022-10-30 17:34:02 -0400

    retab user_dma.c

--- Pynq/C/user_dma.c
+++ Pynq/C/user_dma.c
@@ -21,18 +21,18 @@
 
 #ifndef NODEBUG
 #define dprintf(...) do {\
-			fprintf(stderr, "DEBUG: %s:%d: ", __FILE__, __LINE__); \
-			fprintf(stderr, __VA_ARGS__); \
-			} while (0)
+            fprintf(stderr, "DEBUG: %s:%d: ", __FILE__, __LINE__); \
+            fprintf(stderr, __VA_ARGS__); \
+            } while (0)
 #else
 #define dprintf(...) 
 #endif
 
 #define eprintf(...) do { \
-			fprintf (stderr, "ERROR: %s:%d: ", __FILE__, __LINE__); \
-			fprintf (stderr, __VA_ARGS__); \
-			exit(EXIT_FAILURE); \
-			} while (0)
+            fprintf (stderr, "ERROR: %s:%d: ", __FILE__, __LINE__); \
+            fprintf (stderr, __VA_ARGS__); \
+            exit(EXIT_FAILURE); \
+            } while (0)
 
 // size found in /sys/class/uio/uio0/maps/map0/size
 const uint32_t UIO_SIZE = 0x00010000;
@@ -53,12 +53,12 @@ volatile uint32_t * MM2S_LENGTH;
 static inline
 void setup_dma( const uint8_t * const BASE_VADDR)
 {
-	dprintf("Setting UIO mapping with VBASE=%p\n", BASE_VADDR);
+    dprintf("Setting UIO mapping with VBASE=%p\n", BASE_VADDR);
 
-	MM2S_DMACR	= (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x0 	);
-	MM2S_DMASR 	= (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x4 	);
-	MM2S_SA 	= (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x18	);
-	MM2S_LENGTH	= (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x28	);
+    MM2S_DMACR  = (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x0    );
+    MM2S_DMASR  = (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x4    );
+    MM2S_SA     = (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x18   );
+    MM2S_LENGTH = (uint32_t*) (BASE_VADDR + DMA_OFFSET + 0x28   );
 }
 
 
@@ -67,55 +67,55 @@ void setup_dma( const uint8_t * const BASE_VADDR)
 static
 void dma_tx( const uint32_t * const SRC_PADDR, const uint32_t SRC_SIZE )
 {
-	
-	// Following "Programming Sequence - Direct Register Mode (Simple DMA)" of
-	// https://www.xilinx.com/support/documentation/ip_documentation/axi_dma/v7_1/pg021_axi_dma.pdf
-	
-	dprintf("Starting DMA transfer: %d bytes from %p\n", SRC_SIZE, SRC_PADDR);
+    
+    // Following "Programming Sequence - Direct Register Mode (Simple DMA)" of
+    // https://www.xilinx.com/support/documentation/ip_documentation/axi_dma/v7_1/pg021_axi_dma.pdf
+    
+    dprintf("Starting DMA transfer: %d bytes from %p\n", SRC_SIZE, SRC_PADDR);
     
     //TODO: Steps 1,3, and 4
-	
-	//Step 1:  Start MM2S Channel RS bit
-	
+    
+    //Step 1:  Start MM2S Channel RS bit
+    
     //spin until halt bit deasserts
-	
-	//Step 2 (Interrupts) skipped
-	
-	//Step 3:  Set Source Address
+    
+    //Step 2 (Interrupts) skipped
+    
+    //Step 3:  Set Source Address
 
-	//Step 4:  Set Number of Bytes to transfer
-	// From Docs:  Writing a non-zero value to this register starts the MM2S transfer.
+    //Step 4:  Set Number of Bytes to transfer
+    // From Docs:  Writing a non-zero value to this register starts the MM2S transfer.
 
-	//Wait for completion
+    //Wait for completion
 
-	dprintf("DMA transfer complete\n");
+    dprintf("DMA transfer complete\n");
 
-}	
+}   
 
 uint8_t * get_uio_vaddr()
 {
-    // open+mmap UIO	
-	dprintf("Mapping UIO0\n");
-	int uio_fd = open("/dev/uio0", O_RDWR|O_SYNC);
-	if (uio_fd < 0) eprintf("Error opening /dev/uio0. \n");
-	uint8_t * uio= (uint8_t *) mmap(0x0, UIO_SIZE, PROT_READ|PROT_WRITE, 
-					MAP_SHARED, uio_fd, 0x0);
-	if (uio == NULL) eprintf("Error mmap-ing /dev/uio0.\n");
-	close(uio_fd);
+    // open+mmap UIO    
+    dprintf("Mapping UIO0\n");
+    int uio_fd = open("/dev/uio0", O_RDWR|O_SYNC);
+    if (uio_fd < 0) eprintf("Error opening /dev/uio0. \n");
+    uint8_t * uio= (uint8_t *) mmap(0x0, UIO_SIZE, PROT_READ|PROT_WRITE, 
+                    MAP_SHARED, uio_fd, 0x0);
+    if (uio == NULL) eprintf("Error mmap-ing /dev/uio0.\n");
+    close(uio_fd);
     
     return uio;
 }
 
 uint8_t * get_udma_vaddr( uint32_t udma_size)
 {
-	//open+mmap UDMABUF
-	dprintf("Mapping UDMABUF0\n");
-	int udma_fd = open("/dev/udmabuf0", O_RDWR|O_SYNC);
-	if (udma_fd < 0) eprintf("Error opening /dev/udmabuf0. \n");
-	uint8_t * udma_buf = (uint8_t *) mmap(0x0, udma_size, PROT_READ|PROT_WRITE, 
-					MAP_SHARED, udma_fd, 0x0);
-	if (udma_buf == NULL) eprintf("Error mmap-ing /dev/udmabuf0.\n");
-	close(udma_fd);
+    //open+mmap UDMABUF
+    dprintf("Mapping UDMABUF0\n");
+    int udma_fd = open("/dev/udmabuf0", O_RDWR|O_SYNC);
+    if (udma_fd < 0) eprintf("Error opening /dev/udmabuf0. \n");
+    uint8_t * udma_buf = (uint8_t *) mmap(0x0, udma_size, PROT_READ|PROT_WRITE, 
+                    MAP_SHARED, udma_fd, 0x0);
+    if (udma_buf == NULL) eprintf("Error mmap-ing /dev/udmabuf0.\n");
+    close(udma_fd);
 
     return udma_buf;
 }    
@@ -123,16 +123,16 @@ uint8_t * get_udma_vaddr( uint32_t udma_size)
 uint8_t * get_udma_paddr()
 {
     //look up physical address of udmabuf0
-	dprintf("Loading UDMABUF0 Physical Address\n");
-	int paddr_fd = open("/sys/class/u-dma-buf/udmabuf0/phys_addr", 
+    dprintf("Loading UDMABUF0 Physical Address\n");
+    int paddr_fd = open("/sys/class/u-dma-buf/udmabuf0/phys_addr", 
                 O_RDONLY);
-	if (paddr_fd < 0) eprintf("Error opening phys_addr.\n");
-	char pbuf[100];
-	ssize_t bytes = read(paddr_fd, &pbuf, 100);
-	if (bytes == 0) eprintf("Error reading phys_addr\n");
-	uint8_t * udma_paddr = (uint8_t*) strtol(pbuf, NULL, 16);  //hex
-	dprintf("udmabuf0 Phys Addr: %p\n", udma_paddr);
-	close(paddr_fd);
+    if (paddr_fd < 0) eprintf("Error opening phys_addr.\n");
+    char pbuf[100];
+    ssize_t bytes = read(paddr_fd, &pbuf, 100);
+    if (bytes == 0) eprintf("Error reading phys_addr\n");
+    uint8_t * udma_paddr = (uint8_t*) strtol(pbuf, NULL, 16);  //hex
+    dprintf("udmabuf0 Phys Addr: %p\n", udma_paddr);
+    close(paddr_fd);
 
     return udma_paddr;
 }
@@ -140,28 +140,28 @@ uint8_t * get_udma_paddr()
 uint32_t get_udma_size()
 {
     //look up size of udmabuf0
-	dprintf("Loading UDMABUF0 Size\n");
-	int size_fd = open("/sys/class/u-dma-buf/udmabuf0/size", O_RDONLY);
-	if (size_fd < 0) eprintf("Error opening size.\n");
-	//reuse pbuf and bytes
-	char pbuf[100];
-	ssize_t bytes = read(size_fd, &pbuf, 100);
-	if (bytes == 0) eprintf("Error reading size\n");
-	uint32_t udma_size = strtol(pbuf, NULL, 10);  //decimal
-	dprintf("udmabuf0 Size: %d\n", udma_size);
-	close(size_fd);
+    dprintf("Loading UDMABUF0 Size\n");
+    int size_fd = open("/sys/class/u-dma-buf/udmabuf0/size", O_RDONLY);
+    if (size_fd < 0) eprintf("Error opening size.\n");
+    //reuse pbuf and bytes
+    char pbuf[100];
+    ssize_t bytes = read(size_fd, &pbuf, 100);
+    if (bytes == 0) eprintf("Error reading size\n");
+    uint32_t udma_size = strtol(pbuf, NULL, 10);  //decimal
+    dprintf("udmabuf0 Size: %d\n", udma_size);
+    close(size_fd);
     
     return udma_size;
 }
 
 int main(int argc, char **argv)
 {
-	
-	// check for input file
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s input_filename\n", argv[0]);
-		exit(1);
-	}
+    
+    // check for input file
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s input_filename\n", argv[0]);
+        exit(1);
+    }
 
     uint32_t max_udma_size = get_udma_size();
     //use whichever is the smallest, the DMA buffer or the DMA Transfer Burst Length
@@ -172,39 +172,39 @@ int main(int argc, char **argv)
 
     uint8_t * uio_vaddr = get_uio_vaddr();
 
-	dprintf("Setting UIO mapping\n");
+    dprintf("Setting UIO mapping\n");
     uint32_t * reset_reg = (uint32_t*) uio_vaddr;
     uint32_t * count_reg = (uint32_t*) (uio_vaddr + 0x4);
-	setup_dma( uio_vaddr);
+    setup_dma( uio_vaddr);
 
-	dprintf("Opening input file: %s\n", argv[1]);	
+    dprintf("Opening input file: %s\n", argv[1]);   
     //open the input file 
-	int input_fd = open(argv[1], O_RDONLY);
-	if (input_fd < 0) {
-		fprintf(stderr, "open() %s: %s\n", argv[1], strerror(errno));
-		exit(1);
-	}
+    int input_fd = open(argv[1], O_RDONLY);
+    if (input_fd < 0) {
+        fprintf(stderr, "open() %s: %s\n", argv[1], strerror(errno));
+        exit(1);
+    }
 
-	dprintf("Resetting Count\n");
-	*reset_reg = 1;
+    dprintf("Resetting Count\n");
+    *reset_reg = 1;
 
-	dprintf("Reading input file\n");
+    dprintf("Reading input file\n");
     uint8_t * data_ptr;
-	ssize_t buf_cnt;
+    ssize_t buf_cnt;
     
     //TODO: read file in chunks of udma_size and send the chunks to popcount module using dma_tx function
 
-	
     
     
     
-	// Read the bit count result
-	printf("Counted %u ones\n", *count_reg);
+    
+    // Read the bit count result
+    printf("Counted %u ones\n", *count_reg);
 
-	dprintf("Cleaning up\n"); 
-	close(input_fd);
-	munmap(uio_vaddr, UIO_SIZE);
-	munmap(udma_buf_vaddr, udma_size);
-	return 0;
+    dprintf("Cleaning up\n"); 
+    close(input_fd);
+    munmap(uio_vaddr, UIO_SIZE);
+    munmap(udma_buf_vaddr, udma_size);
+    return 0;
 }
 
